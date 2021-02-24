aeea76d3daedd151af02ae517976961b3fddf6bb
praneethchowdary <67411156+praneethchowdary@users.noreply.github.com>
2021-02-24 17:12:36 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ ls -lrt
 sh /Users/praneethchowdary/Downloads/apache-tomcat-8.5.63/bin/shutdown.sh
 rm -rf /Users/praneethchowdary/Downloads/apache-tomcat-8.5.63/webapps/*.war
 cp target/*.war /Users/praneethchowdary/Downloads/apache-tomcat-8.5.63/webapps
-sh /Users/praneethchowdary/Downloads/apache-tomcat-8.5.63/bin/startup.sh
+cd /Users/praneethchowdary/Downloads/apache-tomcat-8.5.63/bin
+sh startup.sh
